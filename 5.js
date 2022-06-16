ac0ba08f510d2aa8c9c21c6167c72e26b812c74a
SANTOSH MONDAL <santoshmondal@users.noreply.github.com>
2022-06-16 11:26:54 +0530

    ajax

---
+++ 5.js
@@ -0,0 +1,13 @@
+function getData() {
+  const url = "https://jsonplaceholder.typicode.com/posts";
+
+  let promise = fetch(url);
+  promise
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      console.log(data);
+      // logic of dom
+    });
+}
---
+++ 6.js
@@ -0,0 +1,15 @@
+async function getData() {
+  const url = "https://jsonplaceholder.typicode.com/posts";
+
+  // Async call
+  let res = await fetch(url);
+  let list = await res.json();
+
+  // dom operation
+  for (let item of list) {
+    let ref = document.querySelector("#output");
+
+    let newValue = `<p class="post">${item.body}</p>`;
+    ref.innerHTML = newValue + ref.innerHTML;
+  }
+}
