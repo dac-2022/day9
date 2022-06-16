422b66f3509030783ce18a44c73fd583d6bf2590
SANTOSH MONDAL <santoshmondal@users.noreply.github.com>
2022-06-16 10:10:20 +0530

    git remote add origin https://github.com/dac-2022/day9.git

---
+++ 3.js
@@ -0,0 +1,20 @@
+function getData() {
+  const url = "https://jsonplaceholder.typicode.com/posts";
+
+  let http = new XMLHttpRequest();
+
+  // Registering the handler
+  http.onload = function () {
+    const list = JSON.parse(http.responseText);
+
+    let ref = document.querySelector("#output");
+    for (let item of list) {
+      let newValue = `<p class="post">${item.body}</p>`;
+      ref.innerHTML = newValue + ref.innerHTML;
+    }
+  };
+
+  // Sending the request
+  http.open("GET", url, true);
+  http.send();
+}
