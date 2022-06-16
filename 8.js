5974d3b4252d87f658a77d884b424e5c8bf77b59
SANTOSH MONDAL <santoshmondal@users.noreply.github.com>
2022-06-16 11:32:24 +0530

    8.

---
+++ 8.js
@@ -0,0 +1,11 @@
+async function getWeather() {
+  const city = "mumbai";
+  const appid = "7023923dd26a725da995c75b65864de5";
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${appid}&units=metric`;
+
+  let res = await fetch(url);
+  let item = await res.json();
+
+  console.log(item);
+  document.querySelector("#output").innerHTML = item.main.temp;
+}
